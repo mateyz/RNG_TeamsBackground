2849aec5d43e09f2158ecc2a28e5a895c77b45a6
Christopher <91415435+mateyz@users.noreply.github.com>
2022-10-13 14:23:49 +0100

    Added image files

--- __init__.py
+++ __init__.py
@@ -3,11 +3,14 @@ import random
 import dircache
 
 def get_random_image(src):
-    print("Retreiving file from ... ", src)
     filename = random.choice(dircache.listdir(src))
     path = os.path.join(src, filename)
-    print("Selected ... ", path)
-
+    return path
 
 # Find a way to effectively run this function on startup of the Laptop, or per-day.
-get_random_image("./Images")
+if __name__ == "__main__":
+    src = "./Images"
+    dst = "/Users/christopherhutchings/Library/Application Support/Microsoft/Teams/Backgrounds/Uploads"
+    dailyImage = get_random_image(src)
+
+    print("Selected ... ", dailyImage)
