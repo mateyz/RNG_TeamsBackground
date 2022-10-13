5ca02af09b1b8eaea5df7493cf9ea7a8fff8a7fc
Christopher <91415435+mateyz@users.noreply.github.com>
2022-10-13 15:46:54 +0100

    Added changing images

--- __init__.py
+++ __init__.py
@@ -1,13 +1,20 @@
 from genericpath import exists
 import os
 import random
+import shutil
 import dircache
 from datetime import date, datetime
 
 def get_random_image(src):
     filename = random.choice(dircache.listdir(src))
     path = os.path.join(src, filename)
-    return path
+    thumbPath = os.path.join(src + "../ImagesThumbnail", filename)
+    Dict = {
+        "Image" : path,
+        "Thumbnail" : thumbPath
+    }
+
+    return Dict
 
 def is_daytime():
     time = datetime.now()
@@ -23,8 +30,7 @@ if __name__ == "__main__":
     src = "./Images"
     dst = "/Users/christopherhutchings/Library/Application Support/Microsoft/Teams/Backgrounds/Uploads"
     dailyImage = get_random_image(src)
-
-    print("Currently grinding in ... ", dailyImage)
+    print("Currently grinding in ... ", dailyImage["Image"])
 
     # Get the time of day, and determine whether or not the night varient should be used.
     daytime = is_daytime()
@@ -33,6 +39,12 @@ if __name__ == "__main__":
     path = exists(dst + "/Default.jpg")
     if path:
         print("File exists!")
-        os.remove(dst, "Default.jpg")
+        os.remove(dst + "/Default.jpg")
+    #    os.remove(dst + "/Default_thumb.jpg")
 
+    # Copy over the new background files.
+    shutil.copyfile(dailyImage["Image"], dst + "/Default.jpg")
+    
+    
+    # shutil.copyfile(dailyImage["Thumbnail"], dst)
     
