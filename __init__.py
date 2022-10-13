c9d386d2603df2506245ea801f6cd5cb55ce3438
Christopher <91415435+mateyz@users.noreply.github.com>
2022-10-13 14:30:48 +0100

    Added day and night support

--- __init__.py
+++ __init__.py
@@ -1,11 +1,20 @@
 import os
 import random
+from re import L
 import dircache
+from datetime import date, datetime
 
 def get_random_image(src):
     filename = random.choice(dircache.listdir(src))
     path = os.path.join(src, filename)
     return path
+
+def is_daytime():
+    time = datetime.now()
+    hour = time.hour
+    if hour < 15:
+        return True
+    return False
 
 # Find a way to effectively run this function on startup of the Laptop, or per-day.
 if __name__ == "__main__":
@@ -13,4 +22,8 @@ if __name__ == "__main__":
     dst = "/Users/christopherhutchings/Library/Application Support/Microsoft/Teams/Backgrounds/Uploads"
     dailyImage = get_random_image(src)
 
-    print("Selected ... ", dailyImage)
+    print("Currently grinding in ... ", dailyImage)
+
+    # Get the time of day, and determine whether or not the night varient should be used.
+    daytime = is_daytime()
+    print("Is day ", daytime)
