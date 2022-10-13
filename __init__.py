56ee92cb8cf6836769babb225b43289f33c7d67a
Christopher <91415435+mateyz@users.noreply.github.com>
2022-10-13 15:07:13 +0100

    Remove the Default image

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
+from genericpath import exists
 import os
 import random
-from re import L
 import dircache
 from datetime import date, datetime
 
@@ -15,6 +15,9 @@ def is_daytime():
     return hour < 15
     
 
+def strip_image_name(image):
+    return False
+
 # Find a way to effectively run this function on startup of the Laptop, or per-day.
 if __name__ == "__main__":
     src = "./Images"
@@ -25,3 +28,11 @@ if __name__ == "__main__":
 
     # Get the time of day, and determine whether or not the night varient should be used.
     daytime = is_daytime()
+
+    # Check if we already have a Default Image. 
+    path = exists(dst + "/Default.jpg")
+    if path:
+        print("File exists!")
+        os.remove(dst, "Default.jpg")
+
+    
