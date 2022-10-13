dab65b91d9db139d8c4969dbd75e31bd73dd9500
Christopher <91415435+mateyz@users.noreply.github.com>
2022-10-13 11:43:14 +0100

    Added selection
    Added a bunch of test files and a method for selecting a random file from the parent directory.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
-print("Hello")
+import os
+import random
+import dircache
 
-print("Hello number two")
+def get_random_image(src):
+    print("Retreiving file from ... ", src)
+    filename = random.choice(dircache.listdir(src))
+    path = os.path.join(src, filename)
+    print("Selected ... ", path)
+
+
+# Find a way to effectively run this function on startup of the Laptop, or per-day.
+get_random_image("./Images")
