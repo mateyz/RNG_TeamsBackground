b32b94ac4240bb3e0e14443b4f03fab8cc8b48fa
Christopher <91415435+mateyz@users.noreply.github.com>
2022-11-11 12:08:56 +0000

    Implemented persistent Day / Night varients

--- __init__.py
+++ __init__.py
@@ -18,10 +18,18 @@ def get_random_image(src):
     }
     return Dict
 
+def get_specific_image(src, filename):
+    path = os.path.join(src, filename)
+    Dict = {
+        "Name" : filename[0:len(filename) - 4],
+        "Image" : path,
+    }
+    return Dict
+
 def is_daytime():
     time = datetime.now()
     hour = time.hour
-    return hour < 16
+    return hour < 11
 
 def get_image_directory():
     # Search for events (Birthdays, Holidays) and provide cool pictures for them! 
@@ -40,44 +48,56 @@ def strip_image_name(image):
 
 # Find a way to effectively run this function on startup of the Laptop, or per-day.
 if __name__ == "__main__":
-    lastTimeRan = "..."
     lastSceneRan = "..."
 
-    # Identify the previous
+    # Identify the previous image theme
     fp = open("./CurrentScene.txt")
     for i, line in enumerate(fp):
-        if i == 0:
-            lastTimeRan = line.replace("\n", "")
-        elif i == 1:
-            lastSceneRan = line
-    print(lastTimeRan)
-
-    sys.exit()
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    
+        lastSceneRan = line.replace("\n", "")
+        
+    print(lastSceneRan)
     src = get_image_directory()
-    dst = os.path.expanduser('~') + "/Library/Application Support/Microsoft/Teams/Backgrounds/Uploads"
 
-    dailyImage = get_random_image(src)
-    
-    # Logging
-    file_object = open('Logs.txt', 'a')
-    file_object.write('\n\nApp running | ' + current_time)
-    file_object.write('\nCurrently grinding in ... ' + dailyImage["Name"] + " | "+ dailyImage["Image"])
+    if(is_daytime()):
+        print("Generate a new image for the day")
+        newBackground = get_random_image(src)
+    else:
+        print("Generating night varient for current (" + lastSceneRan + ") background")
+        if os.path.exists(src + "/" + lastSceneRan + ".jpg"):
+            newBackground = get_specific_image(src, lastSceneRan + ".jpg")
+        else:
+            print("Night varient not found ... Providing a random nighttime image")
+            newBackground = get_random_image(src)
+
+    # Save the previous image name to our file for later.
+    print(newBackground)
+    persistentFile = open("./CurrentScene.txt", "w")
+    persistentFile.write(newBackground["Name"])
+    persistentFile.close()
+
+    # Force replacing the Default.jpg image
+    dst = os.path.expanduser('~') + "/Library/Application Support/Microsoft/Teams/Backgrounds/Uploads"
 
     # Check if we already have a Default Image. 
     path = exists(dst + "/Default.jpg")
     if path:
-        print("File exists ... Replacing with " + dailyImage["Image"])
+        print("File exists ... Replacing with " + newBackground["Image"])
         os.remove(dst + "/Default.jpg")
     else:
         os.system("osascript -e 'Tell application \"System Events\" to display dialog \" Error: No Default.Jpg selected in Teams...\n Refer to setup guide in ReadMe.md\"'")
         sys.exit()
     # Copy over the new background files.
-    shutil.copyfile(dailyImage["Image"], dst + "/Default.jpg")
+    shutil.copyfile(newBackground["Image"], dst + "/Default.jpg")
     
-    # Close the file
+    # Logging
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    file_object = open('Logs.txt', 'a')
+    file_object.write('\n\nApp running | ' + current_time)
+    file_object.write('\nCurrently grinding in ... ' + newBackground["Name"] + " | "+ newBackground["Image"])
     file_object.close()
+
+    # Exit the current program
+    sys.exit()
     
     
